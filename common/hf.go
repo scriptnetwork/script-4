8297a6803448e90e79d2db6f2a7c8e29300c72be
Manic Beret <manicberet@gmail.com>
2025-01-31 07:29:27 +0000

    parse hf file

--- common/hf.go
+++ common/hf.go
@@ -6,14 +6,20 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+    log "github.com/sirupsen/logrus"
 )
 
 // reward distribution algorithm
 var Height_hf1 uint64
 var Height_hf2 uint64 //tokenomics v2. Licenses
 
+var logger *log.Entry = log.WithFields(log.Fields{"prefix": "hf"})
+
 // Exported function to initialize variables from a file
 func Initialize_hf_values(file_path string) error {
+	logger.Info("Initialize_hf_values")
+
 	// Open the file
 	file, err := os.Open(file_path)
 	if err != nil {
@@ -26,15 +32,16 @@ func Initialize_hf_values(file_path string) error {
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.SplitN(line, "=", 2)
+    	logger.Infof("line %v", line)
 		if len(parts) == 2 {
-	        // Check if the line contains the parameter hf1
+            key := strings.TrimSpace(parts[0])
 			value, err := strconv.ParseUint(strings.TrimSpace(parts[1]), 10, 64)
 			if err != nil {
 				return fmt.Errorf("error parsing value: %w", err)
 			}
-	        if parts[0] == "hf1" {
+	        if key == "hf1" {
 				Height_hf1 = value
-			} else if parts[0] == "hf2" {
+			} else if key == "hf2" {
 				Height_hf2 = value
 			}
 		}
